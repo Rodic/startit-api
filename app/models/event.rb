6a8b239aaaad671b1653266bbf4b6800eda161d4
rodic <alec.rodic@gmail.com>
2015-09-25 11:05:41 +0200

    formatting code in Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,7 +5,7 @@ class Event < ActiveRecord::Base
   validates_numericality_of :start_longitude, greater_than_or_equal_to: -180, less_than_or_equal_to: 180
   validates_length_of       :description, maximum: 500
   validates_length_of       :title, maximum: 150
-  validate :start_time_cannot_be_in_the_past
+  validate                  :start_time_cannot_be_in_the_past
 
   def start_time_cannot_be_in_the_past
     if start_time.present? && start_time < Time.now
