4f1fc5af7cfc26d71b21cd0a54ffdc5876d97c3e
rodic <alec.rodic@gmail.com>
2015-10-01 14:54:49 +0200

    enabling user creation in Sessions#create

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -4,6 +4,13 @@ class V1::SessionsController < ApplicationController
 
   def create
     provider = SatellizerProvider.for(params)
-    render json: provider.get_jwt, status: :ok
+
+    user = User.find_or_create_by(provider: provider.get_provider_name, uid: provider.get_uid) do |u|
+      u.username = provider.get_username
+      u.email    = provider.get_email
+    end
+
+    satellizer_token = JWT.encode({id: user.id}, Rails.application.secrets.secret_key_base)
+    render json: { token: satellizer_token }, status: :ok
   end
 end
--- spec/controllers/v1/sessions_controller_spec.rb
+++ spec/controllers/v1/sessions_controller_spec.rb
@@ -3,16 +3,35 @@ require 'rails_helper'
 RSpec.describe V1::SessionsController, type: :controller do
 
   describe "create" do
+
+    let(:provider_name) { "facebook" }
+    let(:username)      { "John Doe" }
+    let(:uid)           { 1 }
+
     before do
       provider = double("provider")
       allow(SatellizerProvider).to receive(:for).and_return(provider)
-      allow(provider).to receive(:get_jwt).and_return({ "token" => "12345" })
+      allow(provider).to receive(:get_provider_name).and_return(provider_name)
+      allow(provider).to receive(:get_uid).and_return(uid)
+      allow(provider).to receive(:get_username).and_return(username)
+      allow(provider).to receive(:get_email).and_return(nil)
+    end
+
+    it "creates new record if user is new" do
+      expect{ post :create, provider: provider_name }.to change{ User.count }.from(0).to(1)
+    end
+
+    it "returns existing user if it exists" do
+      FactoryGirl.create(:user, provider: provider_name, uid: uid, username: username)
+      expect{ post :create, provider: provider_name }.not_to change{ User.count }
     end
 
     it "renders token" do
-      post :create, provider: 'facebook'
-      expect(response).to have_http_status(:ok)
-      expect(JSON.parse(response.body)).to eq({ "token" => "12345" })
+      expect{ post :create, provider: provider_name }.to change{ User.count }.from(0).to(1)
+      id = User.last.id
+      expect(JSON.parse(response.body)).to eq(
+        { "token" => JWT.encode({id: id}, Rails.application.secrets.secret_key_base) }
+      )
     end
   end
 end
