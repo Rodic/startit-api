e3b60e99829e6c2a81784f6130b412503b6fe7b0
rodic <alec.rodic@gmail.com>
2015-10-12 19:44:36 +0200

    changing format of start_time in Event serializer

--- app/serializers/v1/event_serializer.rb
+++ app/serializers/v1/event_serializer.rb
@@ -2,4 +2,8 @@ class V1::EventSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :start_latitude, :start_longitude, :start_time, :type
 
   has_one :creator
+
+  def start_time
+    object.start_time.strftime("%Y %h %d %H:%M:%S")
+  end
 end
