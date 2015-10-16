2e7bcc97bdb104bbe8811238780942b634161645
rodic <alec.rodic@gmail.com>
2015-10-16 12:36:53 +0200

    fixing user serializer so that 'joined_events' are displayed

--- app/serializers/v1/profile_serializer.rb
+++ app/serializers/v1/profile_serializer.rb
@@ -1,5 +1,6 @@
 class V1::ProfileSerializer < ActiveModel::Serializer
-  attributes :id, :username, :email, :latitude, :longitude
+  attributes :id, :username, :email, :latitude, :longitude, :joined_events
 
   has_many :started_events
+  has_many :joined_events
 end
