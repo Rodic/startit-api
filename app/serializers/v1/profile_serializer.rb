5def959ed53cb8063ca36202d48b9dfb66ba5251
rodic <alec.rodic@gmail.com>
2015-10-22 10:12:07 +0200

    adding event serializer to events in 'has_many'

--- app/serializers/v1/profile_serializer.rb
+++ app/serializers/v1/profile_serializer.rb
@@ -1,6 +1,6 @@
 class V1::ProfileSerializer < ActiveModel::Serializer
   attributes :id, :username, :email, :latitude, :longitude, :joined_events
 
-  has_many :started_events
-  has_many :joined_events
+  has_many :started_events, serializer: V1::EventSerializer
+  has_many :joined_events, serializer: V1::EventSerializer
 end
